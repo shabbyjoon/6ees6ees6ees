50837e65e89ad656682273d309c3fa8ff121d82d
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-06 12:23:22 -0700

    implements ForagerBee in ES6

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends HoneyMakerBee {
+  
+  constructor() {
+  	super();
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+
+  forage() {
+  	this.treasureChest.push('treasure');
+  }
 };
